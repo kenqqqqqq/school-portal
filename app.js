566004a0cd4072f8fbdd5ed035f9aac7f74fff45
mkdysn <yuson.markken@gmail.com>
2018-06-05 23:42:14 +0800

    added login and register form

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // required modules
 const express = require('express');
 const path = require('path');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 // express function
 const app = express();
 
 // middlewares
 // express handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // index route
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('index');
+})
+
+// about route
+app.get('/about', function (req, res) {
+  res.render('about');
+})
+
+// login route
+app.get('/users/login', function (req, res) {
+  res.render('users/login');
+})
+
+// register route
+app.get('/users/register', function (req, res) {
+  res.render('users/register');
 })
 
 
@@ -24,6 +39,6 @@ app.get('/', function(req, res){
 const port = 8700;
 
 // listen method for the server
-app.listen(port, function(){
+app.listen(port, function () {
   console.log(`Server running at port ${port}`);
 });
